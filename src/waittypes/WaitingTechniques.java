7dd40bfe51246e53f252825fe784a42bba398c65
automationtrainingcenter <atcsurya@gmail.com>
2019-12-10 08:51:12 +0530

    explicit waits

--- src/waittypes/WaitingTechniques.java
+++ src/waittypes/WaitingTechniques.java
@@ -1,8 +1,8 @@
 package waittypes;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utitlities.BrowserHelper;
 
@@ -25,16 +25,41 @@ public class WaitingTechniques extends BrowserHelper{
 		
 		
 		// implicitlyWait(): is used to wait until an element found.
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
 
 		// setScriptTimeout()
-		driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+//		driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
 		
 		
-		driver.findElement(By.id("email")).sendKeys("hello");
+//		driver.findElement(By.id("email")).sendKeys("hello");
 		
 		
+		// explicit wait: is also known as WebDriverWait. It is a condition based wait
+		// i.e. we can implement any condition and this wait will wait until the amount
+		// time we specified to verify that condition
+		// All these conditions are available as methods in ExcpectedConditions class
+		// we can implement our condition also using a Function or Predicate functional interfaces
+		
+		
+//		Function<WebDriver, WebElement> findEle = new Function<WebDriver, WebElement>() {
+//
+//			@Override
+//			public WebElement apply(WebDriver driver) {
+//				WebElement ele = driver.findElement(By.id("email"));
+//				return ele;
+//			}
+//		};
+		
+		// create WebDriverWait class object
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email"))).sendKeys("email");
+		wait.until(driver -> driver.findElement(By.id("email"))).sendKeys("sunshine");
+		
+//		wait.until()
+		
+		closeBrowser();
 		
 	}
 
